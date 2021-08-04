e591d8c944e4cdfd8b0cedc4f216b530d7bb8867
femi omoshona <femostic4jesus@gmail.com>
2021-08-04 05:55:06 -0700

    initialcommit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY =" ";
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            videos: [],
+            selectedVideo: null
+         };
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+        this.videoSearch('code247foundation');
+    }
+    videoSearch(term){
+        YTSearch({key: API_KEY, term: 'term'}, (videos) => {
+            this.setState({
+                videos:videos,
+                selectedVideo:videos[0]
+            });
+        });   
+    }
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+  render(){
+    return (
+    <div>
+        <SearchBar />
+        <VideoDetail  video={this.state.selectedVideo}/>
+        <VideoList 
+        onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+        videos={this.state.videos} />
+    </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
